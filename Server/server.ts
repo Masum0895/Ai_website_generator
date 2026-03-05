10fa05d1f2f6dbdccf17851c271742670e9b4a37
@Masum0895 <2221793@iub.edu.bd>
2026-03-05 13:41:39 +0600

    API Created

--- Server/server.ts
+++ Server/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors(corsOptions))
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+app.use(express.json({limit:'50mb'}))
+
 // Middleware
 //app.use(cors())
 app.use(express.json());
@@ -23,6 +26,8 @@ const port = process.env.PORT || 3001;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/user', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
