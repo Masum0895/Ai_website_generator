bfc379427810e77001ee2252b882b7436617a035
@Masum0895 <2221793@iub.edu.bd>
2026-03-07 02:22:05 +0600

    Payment Method Added

--- Server/server.ts
+++ Server/server.ts
@@ -5,6 +5,7 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 import userRouter from "./routes/userRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
+import { stripeWebhook } from "./controllers/stripeWebhook.js";
 
 const app = express();
 
@@ -13,6 +14,8 @@ const corsOptions = {
     credentials: true,
 }
 app.use(cors(corsOptions))
+
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
